b93b0e20046d9befbe7f5d5606ff0e5c5f37eb37
bxgj <bwysxl@126.com>
2022-09-29 00:08:29 +0800

    fix: 修复一处屏幕显示bug

--- software/app/lvgl_test/main.c
+++ software/app/lvgl_test/main.c
@@ -148,7 +148,7 @@ void my_disp_flush(lv_disp_drv_t * disp, const lv_area_t * area, lv_color_t * co
     lcd_data.area.x2 = area->x2;
     lcd_data.area.y1 = area->y1;
     lcd_data.area.y2 = area->y2;
-    lcd_data.area.len = (area->x2 - area->x1) * (area->y2 - area->y1) * sizeof(lv_color_t);
+    lcd_data.area.len = (area->x2 - area->x1 + 1) * (area->y2 - area->y1 + 1) * sizeof(lv_color_t);
     lcd_data.area.data = (uint8_t *)color_p;
     ioctl(lcd_fd, SSP_LCD_SET_DRAW_AREA, (unsigned long)&lcd_data);
 
